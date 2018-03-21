e561c2048f4546ef49326b6b5d185dea4559985b
GeorgiusKurli <georgius.e.k@gmail.com>
2018-03-21 16:47:44 +0700

    Second Commit

--- Models.cpp
+++ Models.cpp
@@ -1,99 +1,11 @@
 #include<iostream>
 using namespace std;
 
-class Player
+struct Skill
 {
-	private:
-		string name;
-		int level;
-		int health;
-		int mana;
-		int strength;
-		int speed;
-		int defense;
-		int exp;
-		int potion;
-		
-	public:
-		Player(string name, int level,int health, int mana, int strength, int speed, int defense)
-		{
-			this->name = name;
-			this->level = level;
-			this->health = health;
-			this->mana = mana;
-			this->strength = strength;
-			this->speed = speed;
-			this->defense = defense;
-
-		}
-		string getName()
-		{
-			return this->name;
-		}
-		
-		int getLevel()
-		{
-			return this->level;
-		}
-		
-		int getHealth()
-		{
-			return this->health;
-		}
-		
-		int getMana()
-		{
-			return this->mana;
-		}
-		
-		int getStrength()
-		{
-			return this->strength;
-		}
-		
-		int getSpeed()
-		{
-			return this->speed;
-		}
-		
-		int getDefense()
-		{
-			return this->defense;
-		}
-		
-		void setLevel(int level)
-		{
-			this->level = level;
-		}
-		
-		void setHealth(int health)
-		{
-			this->health = health;
-		}
-				
-		void setMana()
-		{
-			this->mana = mana;
-		}
-		
-		void setStrength(int strength)
-		{
-			
-			this->strength = strength;
-		}
-		
-		void setSpeed(int speed)
-		{
-			
-			this->speed = speed;
-		}
-		
-		void setDefense(int defense)
-		{
-			
-			this->defense = defense;
-		}
-		
+	int damage;
+	int manacost;
+	string name;
 };
 
 class Monster
@@ -131,10 +43,15 @@ class Monster
 				this->strength = level + rand()%3 + 1;
 				this->speed = level + rand()%4 +1;
 				this->defense = level + rand()%3;
-				this->expdrop = level + rand()%2 + 1;
+				this->expdrop = level * 2 + rand()%10;
 				this->moneydrop = level + rand()%10 + 100;
 			}
 			
+		}
+		
+		string getName()
+		{
+			return this->name;
 		}
 		
 		int getTier()
@@ -179,12 +96,30 @@ class Weapon
 	
 	public:
 		
-		Weapon(int tier){
+		Weapon(int tier,int level){
 			if(tier == 1)
 			{
+				
+				int name = rand()%3;
+				
+				if(name == 0)
+				{
+					this->name = "Iron Scimitar";
+				}
+				
+				else if(name == 1)
+				{
+					this->name = "Iron Broadsword";
+				}
+				
+				else if(name == 3)
+				{
+					this->name = "Iron Rapier";
+				}
+				
 				this->tier = tier;
-				this->level = rand()%9 + 1;
-				this->strength = level + rand()%7;
+				this->level = level;
+				this->strength = level + rand()%5;
 				this->value = level * 25 + strength * 50;
 			}
 		}
@@ -228,13 +163,26 @@ class Armor
 	
 	public:
 		
-		Armor(int tier){
+		Armor(int tier, int level){
 			if(tier == 1)
 			{
+				
+				int name = rand()%2;
+				
+				if(name == 0)
+				{
+					this->name = "Iron Chainmail";
+				}
+				
+				else if(name == 1)
+				{
+					this->name = "Bronze Plating";
+				}
+				
 				this->tier = tier;
-				this->level = rand()%9 + 1;
+				this->level = level;
 				this->speed = level + rand()%3 + 1;
-				this->defense = level + rand()%7;
+				this->defense = level + rand()%5;
 				this->value = level * 25 + defense * 50 + speed * 75;
 			}
 		}
@@ -269,3 +217,208 @@ class Armor
 			return this->value;
 		}
 };
+
+class Player
+{
+	private:
+		string name;
+		int level;
+		int maxhealth;
+		int currenthealth;
+		int maxmana;
+		int currentmana;
+		int strength;
+		int speed;
+		int defense;
+		int exp;
+		int healthpotion;
+		int manapotion;
+		Skill skill_list[10];
+		int skillamount;
+		
+	public:
+		Player(string name, int level,int health, int mana, int strength, int speed, int defense)
+		{
+			this->name = name;
+			this->level = level;
+			this->maxhealth = health;
+			this->currenthealth = health;
+			this->currentmana = maxmana;
+			this->strength = strength;
+			this->speed = speed;
+			this->defense = defense;
+			this->healthpotion = 5;
+			this->manapotion = 3;
+			
+			Skill temp;
+			temp.damage = 1;
+			temp.manacost = 0;
+			temp.name = "Strike";
+			this->skill_list[0] = temp;
+			this->skillamount = 1;
+			
+
+		}
+		string getName()
+		{
+			return this->name;
+		}
+		
+		int getLevel()
+		{
+			return this->level;
+		}
+		
+		int getMaxHealth()
+		{
+			return this->maxhealth;
+		}
+		
+		int getCurrentHealth()
+		{
+			return this->currenthealth;
+		}
+		
+		int getMaxMana()
+		{
+			return this->maxmana;
+		}
+		
+		int getCurrentMana()
+		{
+			return this->currentmana;
+		}
+		
+		int getStrength()
+		{
+			return this->strength;
+		}
+		
+		int getSpeed()
+		{
+			return this->speed;
+		}
+		
+		int getDefense()
+		{
+			return this->defense;
+		}
+		
+		void printSkills()
+		{
+			for(int x = 0; x<skillamount;x++)
+			{
+				Skill temp = skill_list[x];
+				cout << (to_string(x + 1) + ". " + temp.name + " Damage: " + to_string(temp.damage) + " Mana Cost: " + to_string(temp.manacost)) << endl;;
+			}
+		}
+		
+		Skill getSkill(int x)
+		{
+			return skill_list[x-1];
+		}
+		
+		
+		void setCurrentHealth(int health)
+		{
+			this->currenthealth = health;
+		}
+				
+		void setCurrentMana(int mana)
+		{
+			this->currentmana = currentmana;
+		}
+		
+		void setStrength(int strength)
+		{
+			
+			this->strength = strength;
+		}
+		
+		void setSpeed(int speed)
+		{
+			
+			this->speed = speed;
+		}
+		
+		void setDefense(int defense)
+		{
+			
+			this->defense = defense;
+		}
+		
+		void checklevelUp()
+		{
+			int tempstat;
+			if((this->level >= 30) == false)
+			{
+				if(this->exp >= level * 5 + 5)
+				{
+					cout << (this->name + "has levelled up!") << endl;
+					this->level = this->level + 1;
+					this->maxhealth += 5;
+					this->currenthealth = maxhealth;
+					this->currentmana = maxmana;
+					
+					tempstat = (1 + rand()%2);
+					cout << ("Strength increased by = " + to_string(tempstat)) << endl;
+					this->strength += tempstat;
+					
+					cout << ("Speed increased by = " + to_string(tempstat)) << endl;
+					tempstat = (1 + rand()%2);
+					this->speed += tempstat;
+					
+					cout << "Defense increased by = " + to_string(tempstat) << endl;
+					tempstat = (1 + rand()%2);
+					this->defense += tempstat;
+						
+				}	
+			}
+		}
+		
+		bool useHealthPotion()
+		{
+			bool temp;
+			
+			if(this->healthpotion <= 0)
+			{
+				temp = false;
+			}
+			
+			else
+			{
+				this->healthpotion -= 1;
+				this->currenthealth + this->maxhealth * 0.25;
+				if(this->currenthealth > this->maxhealth)
+				{
+					this->currenthealth = this->maxhealth;
+				}
+				temp = true;
+			}
+			return temp;
+		}
+		
+		bool useManaPotion()
+		{
+			bool temp;
+			
+			if(this->manapotion <= 0)
+			{
+				temp = false;
+			}
+			
+			else
+			{
+				this->manapotion -= 1;
+				this->currentmana + this->maxmana * 0.25;
+				if(this->currentmana > this->maxmana)
+				{
+					this->currentmana = this->maxmana;
+				}
+				temp = true;
+			}
+			return temp;
+		}
+		
+};
+
--- View.cpp
+++ View.cpp
@@ -1,7 +1,108 @@
 #include<iostream>
+#include"Controller.cpp"
+#include"Models.cpp"
 using namespace std;
 
-void BattlePhase()
+Player IntroductionPhase()
 {
+	string input,name;
+	cout << "Welcome" << endl;
+	cout << "Please tell us your name." << endl;
+	cin >> input;
+	if(input.length()<0){
+		name = "Noctis";
+		cout << "Not much into names are we? Well then, I will call you Noctis.";
+	}
+	else
+	{
+		name = input;
+		cout << "Welcome to the world " + name;
+	}
+	Player player(name,1,20,100,3,3, 3);
+	return player;
+}
+
+void BattlePhase(int tier, Player player)
+{
+	Monster enemy(tier);
+	int randint;
+	cout << ("A " + enemy.getName() + " appeared!") << endl;
+	bool battleflag = true;
+	string input;
+	bool chooseoption = true;
+	Skill tempskill = (0,0,"Use potion");
+	bool tempbool;
+	string input;
 	
+	while (battleflag)
+	{
+		tempskill = NULL;
+		chooseoption = true;
+		while(chooseoption)
+		{
+			cout << ("1.Fight\n2.Potion\n3.Run") << endl;
+			cin >> input;
+			
+			
+			switch(stoi(input)){
+				case 1:
+				{
+					cout << "0.Back" << endl;
+					player.printSkills();
+					cin >> input;
+					
+					if(stoi(input) != 0)
+					{
+						tempskill = player.getSkill(stoi(input));
+						chooseoption = false;
+					}
+					break;
+				}
+				
+				case 2:
+				{
+					cout << "1.Health Potion 2. Mana Potion 3. Back";
+					cin >> input;
+					
+					if(stoi(input) == 1)
+					{
+						tempbool = player.useHealthPotion();
+						if(tempbool == true)
+						{
+							chooseoption = false;
+						}
+						
+					}
+					
+					if(stoi(input) == 2)
+					{
+						tempbool = player.useManaPotion();
+						if(tempbool == true)
+						{
+							tempskill = (0,0,"Use potion");
+							chooseoption = false;
+						}
+					}
+					
+					break;
+				}
+				
+				case 3:
+				{
+					chooseoption = false;
+					battleflag = false;
+					break;
+				}
+				
+				default:
+				{
+					cout << ("Please input [1..3]!") << endl;
+					break;
+				}
+			}
+		}
+		
+		
+		
+	}
 }
